2570b99139d6054755b384365d9f537c583f2703
Erik27UX <erikfischerux@gmail.com>
2026-03-30 22:16:59 +0200

    Initial Commit

---
+++ Storyloom/StoryloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryloomApp.swift
+//  Storyloom
+//
+//  Created by Erik Fischer on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct StoryloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
